c05aca5a5ba34f27a120c884966055816420b1a6
bosito7 <bosito7@gmail.com>
2016-09-13 21:13:18 -0400

    Python modified Path and KUAL menu fixed

--- runit_KindleClock.sh
+++ runit_KindleClock.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-. /mnt/us/extensions/python/pythonenv
-/mnt/us/extensions/python/AppsPyQT/KindleClock/KindleClock.py
+. /mnt/us/extensions/kindle_Python_PyQt_env/pythonenv
+/mnt/us/extensions/kindle_Python_PyQt_env/AppsPyQT/KindleClock/KindleClock.py
